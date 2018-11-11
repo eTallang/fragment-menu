48dba66c3486a727f55e48ffee970a2ec0a97d39
Erik Tallang <e.tallang@gmail.com>
2018-11-11 08:32:47 +0100

    feat(article): add all required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule
