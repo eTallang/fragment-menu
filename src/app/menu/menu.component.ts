29fc0ad9ced66eb530556402c6c293319c517d7d
Erik Tallang <e.tallang@gmail.com>
2018-11-11 16:15:10 +0100

    feat(fragment-menu): add menu filter

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import * as scrollToElement from 'scroll-to-element';
 
 import { Article } from '../article';
@@ -8,9 +10,24 @@ import { Article } from '../article';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit, OnChanges {
   @Input() articles: Article[] = [];
   @Input() focusedArticle: Article;
+  @ViewChild('filterInput') filterInput: ElementRef<HTMLInputElement>;
+  filteredArticles: Article[] = this.articles;
+
+  ngOnInit() {
+    fromEvent(this.filterInput.nativeElement, 'input').pipe(debounceTime(200)).subscribe((ev: Event) => {
+      const val = this.filterInput.nativeElement.value;
+      this.filteredArticles = this.articles.filter(article => article.title.toLowerCase().indexOf(val.toLowerCase()) > -1);
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.articles) {
+      this.filteredArticles = this.articles;
+    }
+  }
 
   distanceToActive(article: Article): number {
     if (this.focusedArticle) {
